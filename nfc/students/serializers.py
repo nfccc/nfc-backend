c05cf066b56805b7ab96300aebb5faeb58e49c4a
QUANDEVV <vercelploy@gmail.com>
2024-10-29 21:50:22 +0300

    done

--- nfc/students/serializers.py
+++ nfc/students/serializers.py
@@ -2,8 +2,8 @@ from rest_framework import serializers
 from .models import Student
 
 class StudentSerializer(serializers.ModelSerializer):
-    date_registered = serializers.DateTimeField(format="%Y-%m-%d %H:%M")  # Custom format for display
+    date_registered = serializers.DateTimeField(read_only=True)  # Read-only, so not required in input data
 
     class Meta:
         model = Student
-        fields = ['student_name', 'uid_number', 'parent_contact', 'parent_email', 'student_class']
+        fields = ['student_name', 'uid_number', 'parent_contact', 'parent_email', 'student_class', 'date_registered']
