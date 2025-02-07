86831d3a473234fcc8e53a1c3ed8a7ad9c65af03
QUANDEVV <vercelploy@gmail.com>
2025-02-07 22:24:04 +0300

    done

--- nfc/students/urls.py
+++ nfc/students/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('class/<str:student_class>/', views.get_students_by_class, name='get_students_by_class'),
     path('edit/<int:student_id>/', views.update_student, name='update_student'),
     path('delete/<int:student_id>/', views.delete_student, name='delete_student'),
-    path('delete-all/', views.delete_all_students, name='delete_all_students'),  # ✅ New Route for Deleting All Students
-    path('delete-by-name/<str:student_name>/', views.delete_student_by_name, name='delete_student_by_name'),
     path('nfc-scan/', views.handle_nfc_scan, name='handle_nfc_scan'),
+    
 ]
